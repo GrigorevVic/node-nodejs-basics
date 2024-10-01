c5e40842ded9711de0fb65b9f2b6a2d1477719fc
Grigorev Viktor <4342718@gmail.com>
2024-10-01 14:23:11 +0300

    feat: implement function "copy"

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,30 @@
+import fsp from "fs/promises";
+import { constants } from "fs";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getPath = (fileName) => path.join(__dirname, fileName);
+
+const pathToFile = getPath("./files");
+const pathToFileCopy = getPath("./files_copy");
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    await fsp.access(pathToFileCopy, constants.F_OK);
+    const error = new Error("FS operation failed");
+    console.error(`${error.name}: ${error.message}`);
+  } catch (e) {
+    try {
+      await fsp.cp(pathToFile, pathToFileCopy, { recursive: true });
+      console.log('operation successful!');
+    } catch (e) {
+      const error = new Error("FS operation failed");
+      console.error(`${error.name}: ${error.message}`);
+    }
+  }
 };
 
 await copy();
