d62012ac2cd21e10ab910fabd3996276d7910dc7
Grigorev Viktor <4342718@gmail.com>
2024-10-01 13:26:40 +0300

    feat: implement function "create"

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,34 @@
+import fsp from "fs/promises";
+import fs from "fs";
+import { constants } from "fs";
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getPath = (fileName="./files/fresh.txt") => path.join(__dirname, fileName);
+const pathToFile = getPath();
+
 const create = async () => {
-    // Write your code here 
+  fsp
+    .access(pathToFile, constants.F_OK)
+    .then(() => {
+      try {
+        throw new Error("FS operation failed");
+      } catch (e) {
+        console.error(`${e.name}: ${e.message}`);
+      }
+    })
+    .catch(() => {
+      fs.open(pathToFile, "w", (err) => {
+        if (err) throw err;
+      });
+      fs.writeFile(pathToFile, "I am fresh and young", (err) => {
+        if (err) throw err;
+        console.log("File created!");
+      });
+    });
 };
 
 await create();
