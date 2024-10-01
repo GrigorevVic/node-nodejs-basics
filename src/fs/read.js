f34eb3a3d1ca44781183b9f80b6d65c90488dbba
Grigorev Viktor <4342718@gmail.com>
2024-10-01 15:07:57 +0300

    feat: implement function "read"

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import fsp from "fs/promises";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getPath = (fileName) => path.join(__dirname, fileName);
+const pathToFile = getPath("./files/fileToRead.txt");
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const list = await fsp.readFile(pathToFile, { encoding: 'utf8' });
+        console.log(list);
+      } catch (e) {
+        const error = new Error("FS operation failed");
+        console.error(`${error.name}: ${error.message}`);
+      }
 };
 
 await read();
