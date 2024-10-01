b078ea4bda0ae0929306c8e820a1e9e076e240a7
Grigorev Viktor <4342718@gmail.com>
2024-10-01 15:04:36 +0300

    feat: implement function "list"

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import fsp from "fs/promises";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getPath = (fileName) => path.join(__dirname, fileName);
+const pathToDir = getPath("./files");
+
 const list = async () => {
-    // Write your code here 
+    try {
+        const list = await fsp.readdir(pathToDir);
+        console.log(list);
+      } catch (e) {
+        const error = new Error("FS operation failed");
+        console.error(`${error.name}: ${error.message}`);
+      }
 };
 
 await list();
