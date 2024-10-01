677a38afde17d92f51c6a2a8387476551d2a1f52
Grigorev Viktor <4342718@gmail.com>
2024-10-01 14:46:41 +0300

    feat: implement function "rename"

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,30 @@
+import fsp from "fs/promises";
+import { constants } from "fs";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getPath = (fileName) => path.join(__dirname, fileName);
+
+const pathToFile = getPath("./files/wrongFilename.txt");
+const pathToFileRename = getPath("./files/properFilename.md");
+
 const rename = async () => {
-    // Write your code here 
+  try {
+    await fsp.access(pathToFileRename, constants.F_OK);
+    const error = new Error("FS operation failed");
+    console.error(`${error.name}: ${error.message}`);
+  } catch (e) {
+    try {
+      await fsp.rename(pathToFile, pathToFileRename);
+      console.log("operation successful!");
+    } catch (e) {
+      const error = new Error("FS operation failed");
+      console.error(`${error.name}: ${error.message}`);
+    }
+  }
 };
 
 await rename();
