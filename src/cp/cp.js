9be3567a7b87afcb11ee581691fd34baf1987ae9
Grigorev Viktor <4342718@gmail.com>
2024-10-06 18:26:50 +0300

    feat: immplement function spawnChildProces

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,12 @@
+import cp from 'child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = cp.fork('./src/cp/files/script.js', args);
+
+    child.on('exit', () => {
+        console.log('child terminated')
+    })
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['Jon Snow', 'Ned Stark', 'Sansa Stark']);
