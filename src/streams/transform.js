15e32e47f40b633b35e70337a1fc0785497dbac1
Grigorev Viktor <4342718@gmail.com>
2024-10-06 16:50:32 +0300

    feat: implement function "streams/transform"

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,10 +1,12 @@
+import { Transform } from "stream";
+
 const transform = async () => {
-    
-    return new TransformStream({
-        transform(chunk, controller) {
-          controller.enqueue(chunk.toUpperCase());
-        },
-      });
+  const reverse = new Transform({
+    transform(chank, encoding, cb) {
+      cb(null, chank.toString().split("").reverse().join(""));
+    },
+  });
+  process.stdin.pipe(reverse).pipe(process.stdout);
 };
 
 await transform();
