ced0091cb5846eed27f650664c7e7f1caf46e672
Grigorev Viktor <4342718@gmail.com>
2024-10-06 13:02:29 +0300

    feat: implement function "calculateHash"

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,12 @@
+import fs from 'fs';
+import crypto from 'crypto';
+import stream from 'stream/promises';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = crypto.createHash('sha256');
+  const readableStream = fs.createReadStream('./src/hash/files/fileToCalculateHashFor.txt');
+  await stream.pipeline(readableStream, hash);
+  console.log(hash.digest('hex'));
 };
 
 await calculateHash();
